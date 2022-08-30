370e3442e61ad0b207067433925e6f32aa8a5dc2
hsiaokang <hsiaokang@163.com>
2022-08-30 12:27:09 +0800

    VIII: Hooks

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ function commitWork(fiber) {
         domParentFiber = domParentFiber.parent
     }
     const domParent = domParentFiber.dom
+
     if (fiber.effectTag === "PLACEMENT" && fiber.dom != null) {
         domParent.appendChild(fiber.dom)
     } else if (fiber.effectTag === "UPDATE" && fiber.dom != null) {
@@ -99,6 +100,7 @@ function commitWork(fiber) {
     commitWork(fiber.child)
     commitWork(fiber.sibling)
 }
+
 function commitDeletion(fiber, domParent) {
     if (fiber.dom) {
         domParent.removeChild(fiber.dom)
@@ -147,7 +149,6 @@ function performUnitOfWork(fiber) {
     } else {
         updateHostComponent(fiber)
     }
-
     if (fiber.child) {
         return fiber.child
     }
@@ -160,9 +161,44 @@ function performUnitOfWork(fiber) {
     }
 }
 
+let wipFiber = null
+let hookIndex = null
+
 function updateFunctionComponent(fiber) {
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
     const children = [fiber.type(fiber.props)]
     reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+    const oldHook =
+        wipFiber.alternate && wipFiber.alternate.hooks && wipFiber.alternate.hooks[hookIndex]
+    const hook = {
+        state: oldHook ? oldHook.state : initial,
+        queue: [],
+    }
+
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach((action) => {
+        hook.state = action(hook.state)
+    })
+
+    const setState = (action) => {
+        hook.queue.push(action)
+        wipRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot,
+        }
+        nextUnitOfWork = wipRoot
+        deletions = []
+    }
+
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
@@ -226,12 +262,18 @@ function reconcileChildren(wipFiber, elements) {
 const Didact = {
     createElement,
     render,
+    useState,
 }
 
 /** @jsx Didact.createElement */
-function App(props) {
-    return <h1>Hi {props.name}</h1>
+function Counter() {
+    const [state, setState] = Didact.useState(1)
+    return (
+        <h1 onClick={() => setState((c) => c + 1)} style="user-select: none">
+            Count: {state}
+        </h1>
+    )
 }
-const element = <App name="foo" />
+const element = <Counter />
 const container = document.getElementById("root")
 Didact.render(element, container)
